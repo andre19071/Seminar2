8399ef342303bac0d06f194204e453c0e219f4df
Наталья <109668015+andre19071@users.noreply.github.com>
2022-08-13 19:56:00 +0700

    task9

--- task9/Program.cs
+++ task9/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 9. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру чисел.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+int number = new Random().Next(10, 99);
+Console.WriteLine(number);  // Для вывода случайной цифры в ответе можно заменить на команду:
+int digit2 = number % 10;   //Console.WriteLine($ "Наше случайное {number} число");
+int digit1 = number / 10;   // называетс эта операция интерполяция
+if (digit1 > digit2)
+{
+    Console.Write(digit1);
+}
+else
+{
+    Console.Write(digit2);
+}
+Console.WriteLine("-наибольшая цифра");
+
+//void Foo()        // Невозвратный метод
+//{
+//    Console.Write("Невозвратный метод вызван");
+//}
+//int returnFoo()   // Возвратный метод:отличия void ничего нельзя записать слевой стороны
+//{
+//    return 1;
